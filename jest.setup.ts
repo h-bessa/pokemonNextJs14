41a51ef19c53145a0c672762dc71cd5f41d6d39c
Hydris Bessa <hbessa@hevaweb.com>
2024-10-27 10:44:25 +0100

    chore: update dependencies

---
+++ jest.setup.ts
@@ -0,0 +1,12 @@
+import '@testing-library/jest-dom';
+
+global.window.matchMedia = global.window.matchMedia || function() {
+  return {
+    matches: false,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  };
+};
